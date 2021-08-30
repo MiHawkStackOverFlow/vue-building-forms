7d46eef412c5832e34dd7d2670f76dac91149147
Abhishek Sharma <abhivsharma8@gmail.com>
2021-08-30 21:46:25 +0530

    Other uses for binding

--- src/App.vue
+++ src/App.vue
@@ -8,34 +8,34 @@
           <div><strong>Shipping Information</strong></div>
           <div class="form-group">
             <label for="name">Name</label>
-            <input type="text" id="name" class="form-control" placeholder="Your Name" v-model="payment.fullName" />
+            <input type="text" id="name" class="form-control" placeholder="Your Name" v-model="payment.shipping.fullName" />
           </div>
           <div class="form-group">
             <label for="company">Company Name</label>
-            <input type="text" id="company" class="form-control" placeholder="Your Company" v-model="payment.company" />
+            <input type="text" id="company" class="form-control" placeholder="Your Company" v-model="payment.shipping.company" />
           </div>
           <div class="form-group">
             <label for="address1">Address</label>
-            <input type="text" id="address1" class="form-control" placeholder="Street Address" v-model="payment.address1"/>
+            <input type="text" id="address1" class="form-control" placeholder="Street Address" v-model="payment.shipping.address1" />
           </div>
           <div class="form-group">
             <label for="address2">Suite / Apartment #</label>
-            <input type="text" id="address2" class="form-control" placeholder="" v-model="payment.address2"/>
+            <input type="text" id="address2" class="form-control" placeholder="" v-model="payment.shipping.address2" />
           </div>
           <div class="form-row">
             <div class="form-group col-md-6">
               <label for="cityTown">City</label>
-              <input type="text" id="cityTown" class="form-control" placeholder="e.g New York" v-model="payment.cityTown"/>
+              <input type="text" id="cityTown" class="form-control" placeholder="e.g New York" v-model="payment.shipping.cityTown"/>
             </div>
               <div class="form-group col-md-3">
               <label for="stateProvince">State</label>
-              <select id="stateProvince" class="form-control" v-model="payment.stateProvince">
+              <select id="stateProvince" class="form-control" v-model="payment.shipping.stateProvince">
                 <option v-for="state in states" :key="state.abbreviation" :value="state.abbreviation">{{ stateFormat(state) }}</option>
               </select>
             </div>
             <div class="form-group col-md-3">
               <label for="postalCode">ZipCode</label>
-              <input type="text" id="postalCode" class="form-control" placeholder="e.g 1010" v-model="zipCode"/>
+              <input type="text" id="postalCode" class="form-control" placeholder="e.g 1010" v-model="payment.shipping.postalCode" />
             </div>
             <div class="form-group">
               <input type="submit" value="Next" class="btn btn-success" />
@@ -44,6 +44,34 @@
         </div>
         <div class="col-md-6">
           <div><strong>Billing Information</strong></div>
+          <div class="form-check">
+            <input type="checkbox" id="sameAsShipping" class="form-check-input" v-model="payment.billing.sameAsShipping" />
+            <label for="sameAsShipping" class="form-check-label">Same As Shipping?</label>
+          </div>
+          <div class="form-group">
+            <label for="address1">Address</label>
+            <input type="text" id="address1" class="form-control" placeholder="Street Address" v-model="payment.billing.address1" :disabled="payment.billing.sameAsShipping"/>
+          </div>
+          <div class="form-group">
+            <label for="address2">Suite / Apartment #</label>
+            <input type="text" id="address2" class="form-control" placeholder="" v-model="payment.billing.address2" :disabled="payment.billing.sameAsShipping" />
+          </div>
+          <div class="form-row">
+            <div class="form-group col-md-6">
+              <label for="cityTown">City</label>
+              <input type="text" id="cityTown" class="form-control" placeholder="e.g New York" v-model="payment.billing.cityTown" :disabled="payment.billing.sameAsShipping" />
+            </div>
+              <div class="form-group col-md-3">
+              <label for="stateProvince">State</label>
+              <select id="stateProvince" class="form-control" v-model="payment.billing.stateProvince" :disabled="payment.billing.sameAsShipping">
+                <option v-for="state in states" :key="state.abbreviation" :value="state.abbreviation">{{ stateFormat(state) }}</option>
+              </select>
+            </div>
+            <div class="form-group col-md-3">
+              <label for="postalCode">ZipCode</label>
+              <input type="text" id="postalCode" class="form-control" placeholder="e.g 1010"  v-model="payment.billing.postalCode" :disabled="payment.billing.sameAsShipping" />
+            </div>
+          </div>  
         </div>
       </div>
     </form>
@@ -51,7 +79,7 @@
   </div>
 </template>
 <script>
-import { ref, computed } from "vue";
+import { ref, computed, watch } from "vue";
 import states from "@/lookup/states";
 import formatters from "@/formatters";
 
@@ -59,8 +87,13 @@ export default {
   setup() {
     
     const payment = ref({
-      fullName: 'Abhishek',
-      postalCode: '12345'
+      shipping: {
+        fullName: "Shawn",
+        postalCode: "12345",
+      },
+      billing: {
+        sameAsShipping: false,
+      },
     });
     
     const error = ref("");
@@ -81,6 +114,21 @@ export default {
         }
       },
     });
+
+    watch(
+      // What to watch
+      () => payment.value.billing.sameAsShipping,
+      // What to do
+      () => {
+        if (payment.value.billing.sameAsShipping) {
+          payment.value.billing.address1 = "";
+          payment.value.billing.address2 = "";
+          payment.value.billing.cityTown = "";
+          payment.value.billing.stateProvince = "";
+          payment.value.billing.postalCode = "";
+        }
+      }
+    );
     
     return {
       payment,
